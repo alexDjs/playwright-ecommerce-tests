90f6dfd44db23fe8aa6cfae9aa78c6e3c99da81d
alexDjs <your-email@example.com>
2025-11-04 20:17:09 +0100

    Remove unused test files and update workflow

--- tests/utils/site.ts
+++
@@ -1,2 +0,0 @@
-// Returns the URL to test against. On CI we can set TEST_SITE_URL to point to a local server.
-export const SITE_URL = process.env.TEST_SITE_URL ?? 'http://practice.automationtesting.in/';
